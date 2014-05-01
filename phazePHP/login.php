389ee948beb78020ac93a38ea1c49953bb967b93
guilhermegga <guilhermegga@gmail.com>
2014-05-01 17:11:09 -0300

    projeto phaze
    Aqui iremos mexer a partir de agora!

---
+++ phazePHP/login.php
@@ -0,0 +1,18 @@
+
+<meta charset="UTF-8">
+<form method="post" action="login.php"  >
+    <p>Login: <input name="campLogin" type="text"  />  
+    <p>Senha: <input type="text" name="campSenha"/>
+        <input type="button" name="logar" value="Logar"/>
+
+</form>
+<?php
+if (isset($_POST['logar'])) {
+
+    $login = $_POST['campLogin'];
+    $senha = $_POST['campSenha'];
+    
+    
+    
+}
+?>
