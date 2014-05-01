aa7234fa6025c499e5a78f447a677e9f1d2a2bf3
guilhermegga <guilhermegga@gmail.com>
2014-05-01 18:27:00 -0300

    implementando home de adm
    nao sei css e html direito rpa fazer um layout bacana.. vou pegar da
    internet... ainda buscando

--- phazePHP/admHome.php
+++ phazePHP/admHome.php
@@ -1,2 +1,19 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+<div class="container">
+    
+    <div class="form">
+        
+        
+        Aqui vai estar o formulario
+        
+    </div>
+    
+    
+</div>
+
+
+
 <?php
 echo 'voce esta na pagina Inicial do Administrador Phaze ---- em breve novidades!!!';
