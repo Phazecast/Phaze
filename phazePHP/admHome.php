7c5690b2e1d5716b54fd746b81abc4c0ef0029b6
guilhermegga <guilhermegga@gmail.com>
2014-05-01 18:30:00 -0300

    ainda implementando

--- phazePHP/admHome.php
+++ phazePHP/admHome.php
@@ -1,19 +1,22 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<html
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    </head>
+    <body>
+        <div class="container">
+
+            <div class="form">
 
 
-<div class="container">
-    
-    <div class="form">
-        
-        
-        Aqui vai estar o formulario
-        
-    </div>
-    
-    
-</div>
+                Aqui vai estar o formulario
+
+            </div>
 
 
+        </div>
+    </body>
 
+</html>
 <?php
 echo 'voce esta na pagina Inicial do Administrador Phaze ---- em breve novidades!!!';
