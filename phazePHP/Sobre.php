4c3854ba4137cd1b88009a9564fc925acfe8a07f
joaopnj <joaopnj@icloud.com>
2014-06-23 14:04:31 -0300

    Pequenos Ajustes
    Pequenos ajustes na visualização do menu e sobre..Em breve mais
    atuazações..

--- phazePHP/Sobre.php
+++ phazePHP/Sobre.php
@@ -1,38 +1,78 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>Phaze Sobre</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />        
+        <title>Phaze</title>
 
         <link rel="shortcut icon" href="Imagens/favicon.png" />
+
         <link rel="stylesheet" type="text/css" href="Estilo/estilo_geral.css"/>
+
     </head>
     <body>
         <div id="aplicacao">
-
+                
         <?php 
             include 'topo.php';
         ?>
+        
         <div id="corpo">
             <div id="corpo_interno">
-
-                <h1 class="tituloSecao"> SOBRE </h1>
+                <!-- Conteudo a ser apresentado -->
                 
-                <br>
-                
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Um pouco de Bacon, Tetris e AC/DC.
-                
-                <br>
-                <br>
-                <br>
-                <br>
-                
-            <br>
-            <br>
-                    
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
             </div>
         </div>
-            
             
         <?php 
             include 'rodape.php';
@@ -40,5 +80,5 @@
     
         </div>
     </body>
-</html>
 
+</html>
